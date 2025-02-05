bf746eaa1c78414bb11b6702511c26a77295c3f3
Abdul Mannan <itsmannan56@gmail.com>
2025-02-05 14:41:57 +0500

    Base Url Changed

--- src/utils/useHttp.tsx
+++ src/utils/useHttp.tsx
@@ -6,8 +6,8 @@ import axios, {
 import Swal from "sweetalert2";
 
 export const Request = axios.create({
-  baseURL: "http://localhost:3005",
-  // baseURL: "http://16.170.239.246",
+  // baseURL: "http://localhost:3005",
+  baseURL: "http://16.170.239.246/api",
 });
 
 const useHttp = () => {
